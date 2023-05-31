c5fead7ec2778cf99f7c402239dfe585d1c45a9c
ManuKB <manu.kmh@gmail.com>
2023-05-31 21:37:43 +0530

    Update content.js
    Updated v1

--- content.js
+++ content.js
@@ -2,45 +2,56 @@ hii();
 // chrome.runtime.sendMessage({greeting: "Hello from content script!"}, response => {
 //   console.log(response.farewell);
 // });
+
+var tradeList = [];
+
 setInterval(function () {
   console.log("in loop" + new Date());
 
-  chrome.runtime.sendMessage({ url: "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY" },response => {
+  chrome.runtime.sendMessage(
+    { url: "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY" },
+    (response) => {
       //console.log(response);
       // Do something with the response data
       var date = new Date();
       var timeString = date.toLocaleTimeString();
+      document.getElementsByTagName("html")[0].innerHTML = mainPage().content;
       downloadJSON(response, timeString);
-      var body = document.body;
-      // Replace the body with new HTML content
-      body.innerHTML = "<h1>Hello World!</h1><p>This is the new content.</p>";
+      var strike = getStrikes();
+      populator.populate(".strikePrice", strike);
     }
   );
 }, 60 * 1000);
 
 function downloadJSON(data, filename) {
-  var mr = data;
-  if (Object.keys(mr).length !== 0) {
-    mr.currentPrice = mr.records.underlyingValue;
-    mr.timestamp = mr.records.timestamp;
-    delete mr.records;
-    var nxtStrike = Math.ceil(mr.currentPrice / 50) * 50;
-    var index = mr.filtered.data.findIndex(obj => obj.strikePrice === nxtStrike);
-    var r = [];
-    for (let i = -3; i <= 3; i++) {
-      r.push(mr.filtered.data[index+i]);
-    }
-    mr.total = {};
-    mr.total.CE = mr.filtered.CE;
-    mr.total.PE = mr.filtered.PE;
-    delete mr.filtered;
-    mr.strikePrices = r;
+  if (Object.keys(data).length === 0) {
+    console.error("Response Empty");
+  } else {
+    createTradeList(data);
   }
-  var m = new Blob([JSON.stringify(mr)], { type: "application/json" });
-  var url = URL.createObjectURL(m);
-  var link = document.createElement("a");
-  link.href = url;
-  link.download = filename + ".json";
-  link.click();
-  URL.revokeObjectURL(url);
+
+  //var mr = data;
+  // // if (Object.keys(mr).length !== 0) {
+  // //   mr.currentPrice = mr.records.underlyingValue;
+  // //   mr.timestamp = mr.records.timestamp;
+  // //   delete mr.records;
+  // //   var nxtStrike = Math.ceil(mr.currentPrice / 50) * 50;
+  // //   var index = mr.filtered.data.findIndex(obj => obj.strikePrice === nxtStrike);
+  // //   var r = [];
+  // //   for (let i = -3; i <= 3; i++) {
+  // //     r.push(mr.filtered.data[index+i]);
+  // //   }
+  // //   mr.total = {};
+  // //   mr.total.CE = mr.filtered.CE;
+  // //   mr.total.PE = mr.filtered.PE;
+  // //   delete mr.filtered;
+  // //   mr.strikePrices = r;
+  // // }
+  // var m = new Blob([JSON.stringify(mr)], { type: "application/json" });
+  // var url = URL.createObjectURL(m);
+  // var link = document.createElement("a");
+  // link.href = url;
+  // link.download = filename + ".json";
+  // link.click();
+  // URL.revokeObjectURL(url);
 }
